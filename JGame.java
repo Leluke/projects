dceb0944877dc980f0623ee8d5f82948e5d2d78d
Luccas <Luccas>
2017-01-02 23:13:15 -0200

    Adicionados game mde e restart

--- JGame.java
+++ JGame.java
@@ -35,8 +35,7 @@ public class JGame {
     private static int PIPE_SPAWN_TIME = 4;
     
     private static long lastColourChange;    
-    private static long lastFrame;
-    
+    private static long lastFrame;  
     
     static Font font;
     static TrueTypeFont ttf;
@@ -57,61 +56,44 @@ public class JGame {
         shapes.add(new Box(DISPLAY_WIDTH - PIPE_WIDTH, DISPLAY_HEIGHT - gap_height, gap_height, PIPE_WIDTH));
         shapes.add(new Box(DISPLAY_WIDTH - PIPE_WIDTH, 0, DISPLAY_HEIGHT - gap_height - PIPE_GAP_SIZE, PIPE_WIDTH));
         for (final Box box : shapes) {
-            box.speedX = -0.20;
+            box.speedX = -0.20; //sets the pipe speed
+            
         }
     }
 
-    public static void main(String args[]) throws IOException {
-        try {
-            Display.setDisplayMode(new DisplayMode(DISPLAY_WIDTH, DISPLAY_HEIGHT));
-            Display.setTitle("Luccas Malandro Porps Game");
-            Display.create();
-        } catch (LWJGLException e) {
-            e.printStackTrace();
-            Display.destroy();
-            System.exit(1);
-        }
-        //draw player box        
+    private static double game(){
+        PIPE_SPAWN_TIME = 4;
+        shapes.clear();
         Random rand = new Random();
         Timer pipe_spawn_timer = new Timer();
-        pipe_spawn_timer.set(0);
+        pipe_spawn_timer.reset();
         double playerScore = 0;
         boolean gameOver = false;
         int playerX = DISPLAY_WIDTH / 20;
         int playerY = DISPLAY_HEIGHT / 2;
         Box playerBox = new Box(playerX, playerY, 40, 40);
         int random = (int)(Math.random() *(DISPLAY_HEIGHT  - PIPE_GAP_SIZE - 10)  + 1);
-        spawnPipe((DISPLAY_HEIGHT  - PIPE_GAP_SIZE - 10) - random);
-        
-        
-        
-        glMatrixMode(GL_PROJECTION);
-        glOrtho(0, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0, 1, -1);
-        glMatrixMode(GL_MODELVIEW);
-        lastFrame = getTime();
+        spawnPipe((DISPLAY_HEIGHT  - PIPE_GAP_SIZE - 10) - random);       
 
-
-//        Font awtFont = new Font("Times New Roman", Font.ITALIC, 24);
-//        TrueTypeFont font = new TrueTypeFont(awtFont, true);
-        font = new Font("Verdana", Font.BOLD, 20);
-        ttf = new TrueTypeFont(font, true);
-        
+        lastFrame = getTime();        
+        //gameloop
         while (!Display.isCloseRequested() && !gameOver) {            
             glClear(GL_COLOR_BUFFER_BIT);
-                       
-            double delta = getDelta();     
+            //Gets delta hat is used by the library in order to have animations independent from framerate.
+            double delta = getDelta();
+            //tick the timer used for spawning pipes.
             pipe_spawn_timer.tick();
+            //Check if its time to spawn a new pair of pipes.
             if(pipe_spawn_timer.getTime() >= PIPE_SPAWN_TIME){
                 random = (int)(Math.random() *(DISPLAY_HEIGHT  - PIPE_GAP_SIZE - 10)  + 1);
                 System.out.println(random);
                 spawnPipe((DISPLAY_HEIGHT  - PIPE_GAP_SIZE - 10) - random);
-                pipe_spawn_timer.reset();                
+                pipe_spawn_timer.reset();    
             }
-            
             //Keys and event listening
             while (Keyboard.next()) {
                 if (Keyboard.getEventKey() == Keyboard.KEY_SPACE && Keyboard.getEventKeyState()) {
-                    playerBox.speedY = -0.5;               
+                    playerBox.speedY = -0.5; //makes player jump               
                 }
             }
             if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
@@ -119,7 +101,7 @@ public class JGame {
                 System.exit(0);
             }
             
-            //update pipes on screen, check for collision
+            //update pipes on screen, check for collision with player.
             Iterator<Box> i = shapes.iterator();
             while (i.hasNext()) {
                 Box currentBox = i.next();
@@ -139,7 +121,7 @@ public class JGame {
                     System.out.println("COLLISION BOTTOM RIGH CORNER");
                     gameOver = true;
                 }
-                if(currentBox.x <= -DISPLAY_WIDTH){
+                if(currentBox.x <= -PIPE_WIDTH){
                     playerScore += 0.5;
                     System.out.println(playerScore);
                     i.remove();
@@ -150,10 +132,11 @@ public class JGame {
             }
 
             //Update player on screen
-            playerBox.speedY += 0.025;
+            playerBox.speedY += 0.025; //"gravity"
             playerBox.update(delta);
             playerBox.draw();
             
+            //Checks and changes difficulty settings according to scores.
             if (playerScore == 2){
                 PIPE_SPAWN_TIME = 3;
             }
@@ -166,14 +149,96 @@ public class JGame {
             Display.update();
             Display.sync(60);
         }
-        
-        while(!Display.isCloseRequested()){
+        return playerScore;
+    }
+    
+    private static void displayScores(double playerScore){
+        font = new Font("Verdana", Font.BOLD, 20);
+        ttf = new TrueTypeFont(font, true);
+        boolean quitScoreScreen = false;
+        //display scores.
+        while(!quitScoreScreen){
             glClear(GL_COLOR_BUFFER_BIT);
             GL11.glEnable(GL11.GL_BLEND);
             GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
             ttf.drawString(40, 40, "FINAL SCORE: " + playerScore);
+            
+            while (Keyboard.next()) {
+                if (Keyboard.getEventKey() == Keyboard.KEY_SPACE && Keyboard.getEventKeyState()) {
+                    quitScoreScreen = true;              
+                }
+            }
             Display.update();
-        }        
+            Display.sync(60);
+        }
+        GL11.glDisable(GL11.GL_BLEND);
+    }
+    
+    private static int chooseMode(){        
+        int GAME_MODE = 0;
+        font = new Font("Verdana", Font.BOLD, 20);
+        ttf = new TrueTypeFont(font, true);
+        boolean quitchooseMode = false;
+
+        while(!quitchooseMode){
+            glClear(GL_COLOR_BUFFER_BIT);
+            GL11.glEnable(GL11.GL_BLEND);            
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            ttf.drawString(40, 40, "CHOOSE YOUR DESTINY");
+            ttf.drawString(40, 100, "PRESS SPACE TO START THE GAME");
+            
+            
+            while (Keyboard.next()) {
+                if (Keyboard.getEventKey() == Keyboard.KEY_SPACE && Keyboard.getEventKeyState()) {
+                    quitchooseMode = true;
+                    GAME_MODE = 1;
+                }
+            }
+            Display.update();
+            Display.sync(60);
+        }
+        GL11.glDisable(GL11.GL_BLEND);
+        return GAME_MODE;
+    }
+
+    private static void showSettings(){
+        
+    }
+    
+    public static void main(String args[]) throws IOException {               
+        //GAME_MODE 0 = choose game mode
+        //GAME_MODE 1 = play the game
+        //GAME_MODE 2 = settings/credits/whatever.
+        //mode choice. (credits, start game, etc)
+        
+        do {
+            int GAME_MODE = 0;
+            try {
+                Display.destroy();
+                Display.setDisplayMode(new DisplayMode(DISPLAY_WIDTH, DISPLAY_HEIGHT));
+                Display.setTitle("Luccas Malandro Porps Game");
+                Display.create();
+            } catch (LWJGLException e) {
+                e.printStackTrace();
+                Display.destroy();
+                System.exit(1);
+            }
+        glMatrixMode(GL_PROJECTION);
+        glOrtho(0, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0, 1, -1);
+        glMatrixMode(GL_MODELVIEW);            
+        double playerScore;
+
+        GAME_MODE = chooseMode();
+
+        switch(GAME_MODE){               
+            case 1: playerScore = game();
+                    displayScores(playerScore);
+                    break;
+            case 2: showSettings();
+                    break;
+        }
+        //Display.destroy();            
+        }while (!Display.isCloseRequested());
         Display.destroy();
     }
 
